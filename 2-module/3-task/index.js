acffa4f5cf52ce020db71f55142259a1929f360d
KatnikovAA <katnikov_alexey@mail.ru>
2021-08-17 17:21:11 +0300

    Создание калькулятора

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,18 @@
 let calculator = {
-  // ваш код
+
+  read(a, b){
+      this.a = a;
+      this.b = b;
+  },
+  sum: function(){
+    let answerSum = this.a + this.b;
+    return answerSum;
+  },
+  mul: function(){
+    let answerMul = this.a * this.b;
+    return  answerMul;
+  }
+
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
