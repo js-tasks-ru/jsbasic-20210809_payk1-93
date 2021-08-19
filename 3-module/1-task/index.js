3ab251c4a67e568f18c4a8a324a99a589627c610
KatnikovAA <katnikov_alexey@mail.ru>
2021-08-19 14:43:45 +0300

    Сделал через Find и цикле For

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,10 @@
 function namify(users) {
-  // ваш код...
+  let answer = [];
+for (let i = 0; i < users.length+2; i++) {
+  let result = users.find(item => item.name)
+  answer.push(result.name)
+  users.splice(0, 1);
+}
+users = answer;
+return users;
 }
