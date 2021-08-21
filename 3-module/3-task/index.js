a3aeba111e2da0d628f3edca1c4966011f0a7d50
KatnikovAA <katnikov_alexey@mail.ru>
2021-08-21 20:00:10 +0300

    Удаление из строки - и след сделать заглавной

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,15 @@
 function camelize(str) {
-  // ваш код...
+  arr = str.split('')
+  for (let i = 0; i < arr.length; i++) {
+    
+    if (arr[i] == '-')
+    {
+      arr.splice([i],1);
+      let big = arr[i].toUpperCase();
+      arr[i] = big;
+    }
+  }
+  
+  let str1 = arr.join('')
+  return str1;
 }
