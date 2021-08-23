ded8da6679c384a80f5749e09b451dfad399c259
KatnikovAA <katnikov_alexey@mail.ru>
2021-08-23 11:26:15 +0300

    2 задачи Фильтрация по диапазону и Функция showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let newArr = [];
+  let str1;
+    for (let i = 0; i < users.length; i++) { // перебор значейний и если ключ age больше чем age то значения name и balance добавялем в строку, строку пушим в массив
+      if(users[i].age <= age){
+        str1 = users[i].name
+        str1 = str1 + ", " + users[i].balance
+        newArr.push(str1);
+      }
+    }
+  let str = newArr.join('\n'); // объединить массив в строку через ;
+  return str;
 }
