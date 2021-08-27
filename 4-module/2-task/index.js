bbe2316c31422a26908f4611cf6b7bf90d350181
KatnikovAA <katnikov_alexey@mail.ru>
2021-08-27 18:50:50 +0300

    Выделите ячейки по диагонали

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,17 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const tegtable = table;
+  const tegtbody = table.firstElementChild;
+  
+  let lenghtTable = tegtable.rows.length;
+  let lenghtTbody = tegtbody.rows.length;
+  for(i = 0; i < lenghtTable; i++){
+      for(n = 0; n < lenghtTbody; n++){
+          if (i == n){
+              
+              tegtable.rows[i].cells[n].style.backgroundColor = 'red';
+          }
+      }
+  }
+  return tegtable ;
+
 }
