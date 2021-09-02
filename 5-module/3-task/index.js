7efe6a2a97fede9eeb04ff75b3ffe5b7cb21c7f1
KatnikovAA <katnikov_alexey@mail.ru>
2021-09-02 16:52:06 +0300

    Решение задач 4.2/3 и 5.1/2/3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,18 +2,62 @@ function initCarousel() {
 const rightBut = document.querySelector(".carousel__arrow_right");
 const leftBut = document.querySelector(".carousel__arrow_left ");
 const pic = document.querySelector(".carousel__inner");
-console.log(rightBut);
-console.log(leftBut);
-console.log(pic);
+// const picAll = document.querySelectorAll(".carousel__slide");
+// let displayFlag = pic.hasAttribute('translate') ? 'none' : "";
+// rightBut.style.display = displayFlag;
+
+/**
+ * Цикл по добавлению data атрибута с нумерацей
+ * 
+    for (let i = 0; i < picAll.length; i++) {                
+    picAll[i].setAttribute('data-numberSlide', i); 
+  }
+*/
+if(!pic.hasAttribute('translateX')){
+    leftBut.style.display = 'none';
+}
+
 let transX = 0;
-function rightClick() {
-    transX = transX + 988 + "px";
-    pic.style.transform = 'translateX(+988px)';
+let clickNum = 0;
+function rightClick() { // функция для Клика правой кнопки 
+    clickNum = clickNum + 1;
+    const picId = document.querySelector(".carousel__slide");
+    transX = transX - 500;
+    pic.style.transform = "translateX(" + (transX) + "px)";
+    if(clickNum == 3){
+        rightBut.style.display = 'none';
+    }
+    if(clickNum == 0){
+        leftBut.style.display = 'none';
+    }
+    if(clickNum != 0){
+        leftBut.style.display = '';
+    }
+    if(clickNum != 3){
+        rightBut.style.display = '';
+    }
+
 }
-rightBut.addEventListener("click",leftClick );
-function leftClick() {
-    transX = transX - 988 + "px";
-    pic.style.transform = 'translateX(-988px)';
+rightBut.addEventListener("click",rightClick ); // срабатывание клика
+
+function leftClick() {      // функция для Клика левой кнопки 
+    clickNum = clickNum - 1;
+    transX = transX + 500;
+    pic.style.transform = "translateX(" + (transX) + "px)";
+    if(clickNum == 3){
+        rightBut.style.display = 'none';
+    }
+    if(clickNum == 0){
+        leftBut.style.display = 'none';
+    }
+    if(clickNum != 0){
+        leftBut.style.display = '';
+    }
+    if(clickNum != 3){
+        rightBut.style.display = '';
+    }
+
 }
-leftBut.addEventListener("click",leftClick );
+leftBut.addEventListener("click",leftClick ); // срабатывание клика
+
 }
