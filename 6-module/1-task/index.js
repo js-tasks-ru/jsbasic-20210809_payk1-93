4bfa240477b8684c76277633a69e2612385acc0c
KatnikovAA <katnikov_alexey@mail.ru>
2021-09-04 18:18:34 +0300

    Не пойму как сделать get в классе

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,62 @@
  *      }
  *
  */
-export default class UserTable {
-  constructor(rows) {
+
+export default class UserTable { 
+  constructor(rows) { 
+    this.rows = rows;
   }
-}
+  
+  render(){
+    const tableDOM = document.createElement("TABLE");    
+    let theadTable = `<thead><tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+          </tr></thead>`
+    tableDOM.innerHTML = theadTable
+    tableDOM.innerHTML += this.rows.map(rowsItem => {
+        return `<tr>
+          <td>${rowsItem.name}</td>
+          <td>${rowsItem.age}</td>
+          <td>${rowsItem.salary}</td>
+          <td>${rowsItem.city}</td>
+          <td><button>X</button></td>
+          </tr>`;
+      }).join("");
+  
+    
+    tableDOM.addEventListener(`click`, this.onClick);
+    
+    return  tableDOM;
+    }
+    onClick(event){
+      if(event.target.tagName === "BUTTON"){
+        event.target.parentElement.parentElement.remove();
+      }
+    }
+
+    /**   const theadDom = `<thead><tr>
+    <th>Имя</th>
+    <th>Возраст</th>
+    <th>Зарплата</th>
+    <th>Город</th>
+    <th></th>
+    </tr></thead>`
+    let tbodyDom
+    for (const rowsItem in this.rows){
+      tbodyDom = `<tbody><tr>
+      <td>${rowsItem.name}</td>
+      <td>${rowsItem.age}</td>
+      <td>${rowsItem.salary}</td>
+      <td>${rowsItem.city}</td>
+      <td><button>X</button></td>
+      </tr></tbody>`;
+      return tableDOM.innerHTML = theadDom + tbodyDom;
+ */   
+  }
+
+
+
